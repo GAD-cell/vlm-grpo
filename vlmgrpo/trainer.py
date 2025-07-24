ec92a551d85af0d17caead65b1f1766e82f3561c
GAD-cell <gadsinoue@gmail.com>
2025-07-24 12:51:15 +0200

    test if logits depends of vision inputs

--- vlmgrpo/trainer.py
+++ vlmgrpo/trainer.py
@@ -381,6 +381,19 @@ class VLMGRPOTrainer(GRPOTrainer):
                       image_grid_thw=image_grid_thw, logits_to_keep=logits_to_keep + 1).logits
         logits = logits[:, :-1, :]  # (B, L-1, V), exclude the last logit: it corresponds to the next token pred
 
+        logits_without_vision = model(
+            input_ids=input_ids, attention_mask=attention_mask,
+            logits_to_keep=logits_to_keep + 1
+        ).logits
+
+        logits_with_vision = model(
+            input_ids=input_ids, attention_mask=attention_mask,
+            pixel_values=pixel_values, image_grid_thw=image_grid_thw,
+            logits_to_keep=logits_to_keep + 1
+        ).logits
+
+        diff = (logits_with_vision - logits_without_vision).abs().mean()
+        print(f"[DEBUG] Diff between with/without vision: {diff.item()}")
         input_ids = input_ids[:, -logits_to_keep:]
         # For transformers<=4.48, logits_to_keep argument isn't supported, so here we drop logits ourselves.
         # See https://github.com/huggingface/trl/issues/2770
